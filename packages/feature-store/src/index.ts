ffa51f0e250313942eb0575942e52d711fb77c26
Hybrid Tecnologia Inteligente <contato@hybridtech.com.br>
2026-07-22 15:46:38 -0300

    feat(feature-store): export stores and spatial index

--- packages/feature-store/src/index.ts
+++ packages/feature-store/src/index.ts
@@ -1,3 +1,7 @@
 export * from "./GeoJSON.js";
 export * from "./Transactions.js";
 export * from "./FeatureStore.js";
+export * from "./SourceStore.js";
+export * from "./LayerStore.js";
+export * from "./SelectionStore.js";
+export * from "./FeatureIndex.js";
