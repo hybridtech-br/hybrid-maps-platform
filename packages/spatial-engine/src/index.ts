d542a8947f7e7ef93c9b35aacbff32e90b2d6949
Hybrid Tecnologia Inteligente <contato@hybridtech.com.br>
2026-07-24 07:04:57 -0300

    feat(spatial-engine): export measurement APIs

--- packages/spatial-engine/src/index.ts
+++ packages/spatial-engine/src/index.ts
@@ -8,3 +8,7 @@ export * from './geometry/MultiLineString.js';
 export * from './geometry/MultiPolygon.js';
 export * from './geometry/GeometryCollection.js';
 export * from './geometry/GeometryFactory.js';
+
+export * from './measurement/Distance.js';
+export * from './measurement/Length.js';
+export * from './measurement/Bearing.js';
