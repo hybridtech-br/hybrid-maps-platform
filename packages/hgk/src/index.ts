1e13516ea7e098a986a5633420ca8f1572a4e7e9
Hybrid Tecnologia Inteligente <contato@hybridtech.com.br>
2026-08-10 18:34:42 -0300

    feat(hgk): export Point2 public API

--- packages/hgk/src/index.ts
+++ packages/hgk/src/index.ts
@@ -1,2 +1,3 @@
 export { EPSILON, Precision } from './precision/Precision.js';
 export { Vector2, type Vector2Json } from './algebra/Vector2.js';
+export { Point2, type Point2Json } from './geometry/Point2.js';
