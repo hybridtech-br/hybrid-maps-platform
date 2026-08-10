284bf2eb68755835b00bdcbac2bd55b68ed5b51b
Hybrid Tecnologia Inteligente <contato@hybridtech.com.br>
2026-08-10 18:35:37 -0300

    feat(hgk): export Envelope2 public API

--- packages/hgk/src/index.ts
+++ packages/hgk/src/index.ts
@@ -1,3 +1,4 @@
 export { EPSILON, Precision } from './precision/Precision.js';
 export { Vector2, type Vector2Json } from './algebra/Vector2.js';
 export { Point2, type Point2Json } from './geometry/Point2.js';
+export { Envelope2, type Envelope2Json } from './geometry/Envelope2.js';
